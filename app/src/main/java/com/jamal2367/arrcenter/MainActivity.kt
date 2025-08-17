b33cc02616263d7a7b44c381a4eddfd0a6da41d4
Jamal <15986930+jamal2362@users.noreply.github.com>
2025-08-17 12:05:36 +0200

    Update MainActivity.kt

--- app/src/main/java/com/jamal2367/arrcenter/MainActivity.kt
+++ app/src/main/java/com/jamal2367/arrcenter/MainActivity.kt
@@ -1,19 +1,10 @@
 package com.jamal2367.arrcenter
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.compose.BackHandler
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import com.jamal2367.arrcenter.ui.AppRoot
 import com.jamal2367.arrcenter.ui.theme.AppTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
