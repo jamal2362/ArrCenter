dcf57453d28a054f0eb862de4b27b79ea92ee590
Jamal <15986930+jamal2362@users.noreply.github.com>
2025-09-21 11:53:07 +0200

    Improve app

--- app/src/main/java/com/jamal2367/arrcenter/ui/screens/ServiceScreen.kt
+++ app/src/main/java/com/jamal2367/arrcenter/ui/screens/ServiceScreen.kt
@@ -18,9 +18,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Warning
 import androidx.compose.material3.*
-import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.*
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -52,7 +50,6 @@ fun ServiceScreen(type: ServiceType, backgroundColor: Color, onShowSheet: (() ->
     var isLoading by remember { mutableStateOf(true) }
     var isError by remember { mutableStateOf(false) }
     var webViewRef by remember { mutableStateOf<WebView?>(null) }
-    var isRefreshing by remember { mutableStateOf(false) }
 
     val fileChooserLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.OpenDocument(),
@@ -110,168 +107,133 @@ fun ServiceScreen(type: ServiceType, backgroundColor: Color, onShowSheet: (() ->
     }
 
     Scaffold { innerPadding ->
-        PullToRefreshBox(
-            isRefreshing = isRefreshing,
-            onRefresh = {
-                isRefreshing = true
-                coroutineScope.launch {
-                    loadUrl()
-                    isRefreshing = false
-                }
-            },
-            modifier = Modifier.fillMaxSize()
-        ) {
-            when {
-                isLoading -> Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(backgroundColor)
-                        .padding(innerPadding),
-                    contentAlignment = Alignment.Center
+        when {
+            isLoading -> Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(backgroundColor)
+                    .padding(innerPadding),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+            isError -> Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(backgroundColor)
+                    .padding(24.dp)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Warning,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onPrimaryContainer,
+                    modifier = Modifier.size(80.dp)
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = stringResource(R.string.no_connection),
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    textAlign = TextAlign.Center
+                )
+                Spacer(modifier = Modifier.height(36.dp))
+                Button(
+                    onClick = { coroutineScope.launch {
+                        loadUrl()
+                    } },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.onPrimaryContainer)
                 ) {
-                    CircularProgressIndicator()
+                    Text(text = stringResource(R.string.retry))
                 }
-                isError -> Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(backgroundColor)
-                        .padding(24.dp)
-                        .verticalScroll(rememberScrollState()),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Icon(
-                        imageVector = Icons.Rounded.Warning,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onPrimaryContainer,
-                        modifier = Modifier.size(80.dp)
-                    )
-                    Spacer(modifier = Modifier.height(12.dp))
-                    Text(
-                        text = stringResource(R.string.no_connection),
-                        style = MaterialTheme.typography.headlineSmall,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        modifier = Modifier.padding(horizontal = 16.dp),
-                        textAlign = TextAlign.Center
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Text(
-                        text = stringResource(R.string.refresh),
-                        style = MaterialTheme.typography.bodyMedium,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        textAlign = TextAlign.Center
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(
-                        onClick = {
-                            coroutineScope.launch {
-                                loadUrl()
+            }
+            else -> currentUrl?.let { url ->
+                AndroidView(
+                    modifier = Modifier.fillMaxSize(),
+                    factory = { ctx ->
+                        val swipeRefreshLayout = SwipeRefreshLayout(ctx)
+                        val cookieManager = CookieManager.getInstance()
+
+                        val webView = WebView(ctx).apply {
+                            layoutParams = ViewGroup.LayoutParams(
+                                ViewGroup.LayoutParams.MATCH_PARENT,
+                                ViewGroup.LayoutParams.MATCH_PARENT
+                            )
+                            settings.javaScriptEnabled = true
+                            settings.domStorageEnabled = true
+                            settings.builtInZoomControls = true
+                            settings.displayZoomControls = false
+                            settings.allowFileAccess = true
+                            settings.allowContentAccess = true
+                            settings.setSupportZoom(true)
+
+                            cookieManager.setAcceptCookie(true)
+                            cookieManager.setAcceptThirdPartyCookies(this, true)
+
+                            if (type == ServiceType.SABnzbd || type == ServiceType.Ugreen) {
+                                settings.useWideViewPort = true
+                                settings.loadWithOverviewMode = true
+                                settings.userAgentString = isDesktopMode()
                             }
-                        },
-                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.onPrimaryContainer)
-                    ) {
-                        Text(
-                            text = stringResource(R.string.retry)
-                        )
-                    }
-                } else -> currentUrl?.let { url ->
-                    AndroidView(
-                        modifier = Modifier.fillMaxSize(),
-                        factory = { ctx ->
-                            val swipeRefreshLayout = SwipeRefreshLayout(ctx)
-                            val cookieManager = CookieManager.getInstance()
 
-                            val webView = WebView(ctx).apply {
-                                layoutParams = ViewGroup.LayoutParams(
-                                    ViewGroup.LayoutParams.MATCH_PARENT,
-                                    ViewGroup.LayoutParams.MATCH_PARENT
-                                )
-                                settings.javaScriptEnabled = true
-                                settings.domStorageEnabled = true
-                                settings.builtInZoomControls = true
-                                settings.displayZoomControls = false
-                                settings.allowFileAccess = true
-                                settings.allowContentAccess = true
-                                settings.setSupportZoom(true)
+                            webViewClient = object : WebViewClient() {
+                                override fun shouldOverrideUrlLoading(view: WebView?, request: android.webkit.WebResourceRequest): Boolean {
+                                    val clickedUrl = request.url.toString()
 
-                                cookieManager.setAcceptCookie(true)
-                                cookieManager.setAcceptThirdPartyCookies(this, true)
-
-                                if (type == ServiceType.SABnzbd || type == ServiceType.Ugreen) {
-                                    settings.useWideViewPort = true
-                                    settings.loadWithOverviewMode = true
-                                    settings.userAgentString = isDesktopMode()
+                                    if (clickedUrl.contains("youtube.com") || clickedUrl.contains("youtu.be")) {
+                                        val intent = Intent(Intent.ACTION_VIEW, clickedUrl.toUri())
+                                        intent.setPackage("com.google.android.youtube")
+                                        if (intent.resolveActivity(ctx.packageManager) == null) intent.setPackage(null)
+                                        ctx.startActivity(intent)
+                                        return true
+                                    }
+                                    return false
                                 }
 
-                                webViewClient = object : WebViewClient() {
-                                    override fun shouldOverrideUrlLoading(view: WebView?, request: android.webkit.WebResourceRequest): Boolean {
-                                        val clickedUrl = request.url.toString()
 
-                                        if (clickedUrl.contains("youtube.com") || clickedUrl.contains("youtu.be")) {
-                                            val intent =
-                                                Intent(Intent.ACTION_VIEW, clickedUrl.toUri())
-                                            intent.setPackage("com.google.android.youtube")
+                                override fun onPageFinished(view: WebView?, url: String?) {
+                                    super.onPageFinished(view, url)
+                                    swipeRefreshLayout.isRefreshing = false
 
-                                            if (intent.resolveActivity(ctx.packageManager) == null) {
-                                                intent.setPackage(null)
-                                            }
-
-                                            ctx.startActivity(intent)
-                                            return true
-                                        }
-
-                                        return false
+                                    view?.let {
+                                        injectCSS(it)
                                     }
 
-                                    override fun onPageFinished(view: WebView?, url: String?) {
-                                        super.onPageFinished(view, url)
-                                        swipeRefreshLayout.isRefreshing = false
-                                        isRefreshing = false
-
-                                        view?.let {
-                                            injectCSS(it)
-                                        }
-
-                                        if (type == ServiceType.SABnzbd || type == ServiceType.Ugreen) {
-                                            view?.evaluateJavascript(
-                                                isJS(),
-                                                null
-                                            )
-                                        }
-                                    }
-
-                                    override fun onReceivedError(
-                                        view: WebView?,
-                                        request: android.webkit.WebResourceRequest?,
-                                        error: android.webkit.WebResourceError?
-                                    ) {
-                                        super.onReceivedError(view, request, error)
-                                        isError = true
+                                    if (type == ServiceType.SABnzbd || type == ServiceType.Ugreen) {
+                                        view?.evaluateJavascript(isJS(), null)
                                     }
                                 }
 
-                                webChromeClient = WebChromeClientWithCallback { mimeTypes ->
-                                    fileChooserLauncher.launch(mimeTypes)
+                                override fun onReceivedError(view: WebView?, request: android.webkit.WebResourceRequest?, error: android.webkit.WebResourceError?) {
+                                    super.onReceivedError(view, request, error)
+                                    swipeRefreshLayout.isRefreshing = false
+
+                                    isError = type != ServiceType.Ugreen
                                 }
-
-                                loadUrl(url)
                             }
 
-                            webViewRef = webView
-
-                            swipeRefreshLayout.setOnChildScrollUpCallback { _, _ ->
-                                webView.scrollY > 0
+                            webChromeClient = WebChromeClientWithCallback { mimeTypes ->
+                                fileChooserLauncher.launch(mimeTypes)
                             }
 
-                            swipeRefreshLayout.setOnRefreshListener {
-                                webView.reload()
-                            }
-
-                            swipeRefreshLayout.addView(webView)
-                            swipeRefreshLayout
+                            loadUrl(url)
                         }
-                    )
-                }
+
+                        webViewRef = webView
+
+                        swipeRefreshLayout.setOnRefreshListener {
+                            webView.reload()
+                            swipeRefreshLayout.isRefreshing = true
+                        }
+
+                        swipeRefreshLayout.addView(webView)
+                        swipeRefreshLayout
+                    }
+                )
             }
         }
     }
