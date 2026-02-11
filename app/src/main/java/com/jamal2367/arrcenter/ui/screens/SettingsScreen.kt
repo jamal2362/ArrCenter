d0d26211ee225e5b40ad73a568c45612cc43ac47
jamal2362 <15986930+jamal2362@users.noreply.github.com>
2026-02-11 10:11:10 +0100

    Add modifier to fill max size in SettingsScreen

--- app/src/main/java/com/jamal2367/arrcenter/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/jamal2367/arrcenter/ui/screens/SettingsScreen.kt
@@ -50,6 +50,7 @@ fun SettingsScreen(onSaved: () -> Unit = {}) {
     }
 
     Scaffold { inner ->
+        modifier = Modifier.fillMaxSize()
         Column(
             modifier = Modifier
                 .fillMaxSize()
