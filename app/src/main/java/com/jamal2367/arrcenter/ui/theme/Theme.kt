75f47288f1bf03fa8405cd3f268d86d97a326f1a
Jamal <15986930+jamal2362@users.noreply.github.com>
2026-02-11 19:21:26 +0100

    Use primary color & add dynamic light theme

--- app/src/main/java/com/jamal2367/arrcenter/ui/theme/Theme.kt
+++ app/src/main/java/com/jamal2367/arrcenter/ui/theme/Theme.kt
@@ -1,17 +1,24 @@
 package com.jamal2367.arrcenter.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun AppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
     val context = LocalContext.current
-    val colorScheme = dynamicDarkColorScheme(context)
+    val colorScheme = if (darkTheme) {
+        dynamicDarkColorScheme(context)
+    } else {
+        dynamicLightColorScheme(context)
+    }
 
     MaterialTheme(
         colorScheme = colorScheme,
